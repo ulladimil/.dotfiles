404ccbb5b59d4e716d769c38991891e3faad2f52
ulladimil <ulladimil@gmail.com>
2026-07-30 21:43:49 +0300

    nvim fix git config

--- nvim/lua/keymap.lua
+++ nvim/lua/keymap.lua
@@ -62,12 +62,13 @@ vim.keymap.set('n', '<leader>hs', gitsigns.stage_hunk, {})
 vim.keymap.set('v', '<leader>hs', function()
   gitsigns.stage_hunk({ vim.fn.line('.'), vim.fn.line('v') })
 end, {})
+vim.keymap.set('n', '<leader>hS', gitsigns.stage_buffer, {})
 
-vim.keymap.set('n', '<header>hr', gitsigns.reset_hunk, {})
+vim.keymap.set('n', '<leader>hr', gitsigns.reset_hunk, {})
 vim.keymap.set('v', '<leader>hr', function()
   gitsigns.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') })
 end, {})
-
+vim.keymap.set('n', '<leader>hR', gitsigns.reset_buffer_index, {})
 
 -- Quickfix
 vim.api.nvim_set_keymap('n', '<M-j>', '<CMD>cnext<CR>', { noremap = true, silent = true })
